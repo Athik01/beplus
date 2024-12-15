43b58b5ac8c640c154af1e20681d2b067e772727
Athik2005 <mohamedathikr.22msc@kongu.edu>
2024-12-16 01:02:39 +0530

    My Proper Code

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -3,7 +3,6 @@ import 'package:beplus/order_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 class CartPage extends StatefulWidget {
   final String userId;
 
